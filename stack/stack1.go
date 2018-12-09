3dd20235b35bad251f4bbcd82b233bbbd355bd46
sidharth01g <sidharth01g@gmail.com>
2018-12-09 19:10:42 +0530

    Updated

--- stack/stack1.go
+++ stack/stack1.go
@@ -1,10 +1,23 @@
 package main
 
 // import "fmt"
+func increment_bad(val int) {
+	val ++
+	println("increment_bad - val: ", val, &val)
+}
+
+func increment_good(val *int) {
+	*val++
+	println("increment_good - val: ", *val, val, &val)
+}
 
 func main() {
 	count := 10
-	println("Before: ", count, &count)
+	println("main - Before: ", count, &count)
 	count++
-	println("After", count, &count)
+	println("main - After", count, &count)
+	increment_bad(count)
+	println("main - After", count, &count)
+	increment_good(&count)
+	println("main - After", count, &count)
 }
